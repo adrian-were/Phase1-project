31882f06686344f876668c94df5778ae946acbc1
Adrian <adrianshisia@gmail.com>
2023-07-04 15:54:12 +0300

    Displayed data from server

--- script.js
+++ script.js
@@ -6,5 +6,21 @@ function hotelNames(resorts){
         let list = document.createElement("li")
         list.classList = "place"
         list.innerHTML = resort.name
+        grab.appendChild(list)
     })
 }
+
+// fetch hotel names from JSON server
+function fetchHotelNames(){
+    fetch('http://localhost:3000/hotels')
+    .then(res => res.json())
+    .then(data => {
+        hotelNames(data)
+    })
+}
+
+// Event listener
+document.addEventListener('DOMContentLoaded',(e) => {
+    fetchHotelNames()
+    e.preventDefault()
+})
